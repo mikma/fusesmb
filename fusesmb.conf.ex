c97226a50814f3402376df8dbf306c19b5824b30
Vincent Wagelaar <vincent@ricardis.tudelft.nl>
2005-08-12 14:01:46 +0100

    Sample configuration file

---
+++ fusesmb.conf.ex
@@ -0,0 +1,33 @@
+; Global settings
+;[global]
+; Default username and password
+; username=user
+; password=totallysecret
+
+; List hidden shares
+;showhiddenshares=true
+
+; Connection timeout in seconds
+timeout = 10
+
+;Interval for updating new shares in minutes
+interval = 10
+
+; Don't list these servers and/or workgroups separated by commas
+;[ignore]
+;servers=SERVER,SERVER2,SERVER3
+;workgroups=WORKGROUP,WG2
+
+
+; Share specific settings (server name should start with a slash (/)
+; [/SERVER/SHARE]
+; username=vincent
+; password=banaan
+
+; Server specific settings (server name should start with a slash (/)
+;[/SERVER]
+; username=vincent
+; password=banaan
+; showhiddenshares=true
+; ignore=true
+
