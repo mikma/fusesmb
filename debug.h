248ab65141fae4d25e0ee042b9abbd852968cf51
Vincent Wagelaar <vincent@ricardis.tudelft.nl>
2005-08-15 20:57:42 +0100

    Only use debugging if gcc is used

--- debug.h
+++ debug.h
@@ -1,7 +1,7 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#ifndef __OPTIMIZE__
+#if defined(__GNUC__) && !defined(__OPTIMIZE__)
 #define debug(format, ...) fprintf(stderr, "%s: %s: %d " format "\n", __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
 #define debug(format, ...)
